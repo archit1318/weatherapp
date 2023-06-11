86079af25c840e862bb063b54a2821e8c40613f6
archit1318 <archittiwari7013@gmail.com>
2023-06-12 05:27:55 +0530

    all files

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApioptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '59cda6083dmsh8f14f2623c944f6p12a393jsn8bf373790d85',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+export const GEO_API_URL="https://wft-geo-db.p.rapidapi.com/v1/geo"
+
+export const WEATHER_API_URL="https://api.openweathermap.org/data/2.5";
+
+export const WEATHER_API_KEY="bc13e19c32c53814f18e5d463e54e1a1";
